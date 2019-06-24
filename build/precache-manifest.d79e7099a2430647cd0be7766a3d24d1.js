5f08efa7d161a13889c79434ec41b795272216fc
Code4Blessings <robinwarden@code4blessings.com>
2019-06-23 22:33:34 -0400

    Adding build file

---
+++ build/precache-manifest.d79e7099a2430647cd0be7766a3d24d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0e7cc5893c5cead8b53e28e3da66c6",
+    "url": "/pokemon-project/index.html"
+  },
+  {
+    "revision": "f76fc7f64a20560c932c",
+    "url": "/pokemon-project/static/css/main.1bdde325.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokemon-project/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "f76fc7f64a20560c932c",
+    "url": "/pokemon-project/static/js/main.f45150e0.chunk.js"
+  },
+  {
+    "revision": "3592ce7198d2df1c426a",
+    "url": "/pokemon-project/static/js/runtime~main.74731005.js"
+  }
+]);
